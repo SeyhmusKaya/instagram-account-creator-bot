e275d1d89f9a1bffa0108fecd47731b966f44fea
Şeyhmus Kaya <33554810+SeyhmusKaya@users.noreply.github.com>
2022-07-17 22:10:55 +0300

    .

---
+++ InstaOpener2/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace InstaOpener2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Bot durumu: Pasif";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Başarılı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Başarısız:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 104);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "0";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 148);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "TashMedya Instagram Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ InstaOpener2/Form1.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace InstaOpener2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+        public static int RandNumber(int start, int finish)
+        {
+            Random rnd = new Random();
+            return rnd.Next(start, finish);
+        }
+        public static string RandomString(int length)
+        {
+            Random rnd = new Random();
+            const string chars = "abcdefghjklmnoprstyouvyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+        public static string RandomCharacter(int length)
+        {
+            Random rnd = new Random();
+            const string chars = "abcdefghjklmnoprstyouvyxz";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+        public static string RandomNumber(int length)
+        {
+            Random rnd = new Random();
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[rnd.Next(s.Length)]).ToArray());
+        }
+
+        public static string CreateUsername(String mail)
+        {
+            String username = "";
+            String[] words = mail.Split('@');
+            username = RandomCharacter(2) + words[0] + RandomNumber(2);
+            return username;
+        }
+        private static void WriteFileSuccess(String mail, String password)
+        {
+            /*
+            string filePath = Application.StartupPath.ToString() + "\\successMail.txt"; 
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.Flush(); // write
+            sw.Close();
+            fs.Close();
+            */
+            File.AppendAllText(Application.StartupPath.ToString() + "\\successMail.txt", mail + ":" + password + Environment.NewLine);
+
+        }
+        private static void WriteFileFail(String mail, String password)
+        {
+            File.AppendAllText(Application.StartupPath.ToString() + "\\failMail.txt", mail + ":" + password + Environment.NewLine);
+
+        }
+        private static void WriteInstaAccount(String username, String password,String mail,String fullname)
+        {
+            File.AppendAllText(Application.StartupPath.ToString() + "\\instaUser.txt", username + ":" + password + ":" + mail + ":" + fullname + Environment.NewLine);
+
+        }
+        public void Deneme(String yandexMail, String yandexPass,String instaFullName, String instaPassword,String instaUsername)
+        {
+            label1.Text = "Tarayıcı başlatılıyor...";
+
+            ChromeDriverService _chromeDriverService;
+            ChromeOptions _chromeOptions;
+            IWebDriver driver;
+            IWebDriver yandexDriver;
+
+            //get random proxy 
+            label1.Text = "Rastgele proxy seçiliyor...";
+            var lines = File.ReadAllLines("proxy.txt");
+            var r = new Random();
+            var randomLineNumber = r.Next(0, lines.Length - 1);
+            String line = lines[randomLineNumber];
+            String [] allProxy = line.Split(':');
+            String proxyId = allProxy[0];
+            int proxyPort = int.Parse(allProxy[1]);
+            String proxyUsername = allProxy[2];
+            String proxyPassword = allProxy[3];
+            //end random proxy
+
+            _chromeDriverService = ChromeDriverService.CreateDefaultService();
+            _chromeDriverService.HideCommandPromptWindow = true;
+            _chromeOptions = new ChromeOptions();
+            //_chromeOptions.AddArguments("headless"); // for background
+
+            // _chromeOptions.AddHttpProxy(proxyId, proxyPort, proxyUsername, proxyPassword); //--> if you want 
+            _chromeOptions.AddArgument("ignore-certificate-errors");
+
+
+            Proxy proxy = new Proxy();
+            proxy.Kind = ProxyKind.Manual;
+            proxy.IsAutoDetect = false;
+            proxy.HttpProxy = proxyId + ":" + proxyPort;
+            proxy.SocksUserName = proxyUsername;
+            proxy.SocksPassword = proxyPassword;
+            _chromeOptions.Proxy = proxy;
+
+            driver = new ChromeDriver(_chromeDriverService, _chromeOptions);
+            yandexDriver = new ChromeDriver(_chromeDriverService, _chromeOptions);
+
+            try
+            {
+                label1.Text = "İnstagram kayıt sayfasına gidiliyor...";
+
+                driver.Navigate().GoToUrl("https://www.instagram.com/accounts/emailsignup/?hl=tr");
+                Thread.Sleep(10000);
+                try
+                {
+                    WebElement cerez = (WebElement)driver.FindElement(By.XPath("//button[text()='Sadece temel çerezlere izin ver']"));
+                    cerez.Click();
+                    Thread.Sleep(5000);
+                }
+                catch
+                {
+                    Thread.Sleep(1000);
+                }
+                label1.Text = "Kayıt değerleri giriliyor...";
+                try
+                {
+
+                    WebElement mail = (WebElement)driver.FindElement(By.Name("emailOrPhone"));
+                    mail.SendKeys(yandexMail);
+                }
+                catch
+                {
+                    driver.Navigate().Refresh();
+                    Thread.Sleep(8000);
+                    WebElement mail = (WebElement)driver.FindElement(By.Name("emailOrPhone"));
+                    mail.SendKeys(yandexMail);
+                }
+                label1.Text = "Mail adresi girildi...";
+
+                Thread.Sleep(1256);
+                WebElement fullName = (WebElement)driver.FindElement(By.Name("fullName"));
+                fullName.SendKeys(instaFullName);
+                label1.Text = "İsim girildi...";
+
+                Thread.Sleep(2180);
+                WebElement username = (WebElement)driver.FindElement(By.Name("username"));
+                username.SendKeys(instaUsername);
+                label1.Text = "Kullanıcı adı girildi...";
+
+                Thread.Sleep(1354);
+                WebElement pass = (WebElement)driver.FindElement(By.Name("password"));
+                pass.SendKeys(instaPassword);
+                label1.Text = "Şifre girildi...";
+
+                Thread.Sleep(2856);
+                try
+                {
+                    WebElement clickButtonNext1 = (WebElement)driver.FindElement(By.XPath("//button[text()='Kaydol']"));
+                    clickButtonNext1.Click();
+                }
+                catch
+                {
+                    WebElement clickButtonNext2 = (WebElement)driver.FindElement(By.XPath("//button[text()='İleri']"));
+                    clickButtonNext2.Click();
+                }
+                Thread.Sleep(5856);
+                label1.Text = "Rastgele doğum tarihi giriliyor...";
+                
+                var allDate = driver.FindElements(By.ClassName("h144Z"));
+                // allDate[0] --> month
+                // allDate[1] --> day
+                // allDate[2] --> year
+
+                var selectElement0 = new SelectElement(allDate[0]);
+                selectElement0.SelectByIndex(RandNumber(1, 11));
+                Thread.Sleep(1116);
+
+                var selectElement = new SelectElement(allDate[1]);
+                selectElement.SelectByIndex(RandNumber(1, 28));
+                Thread.Sleep(1240);
+
+                var selectElement2 = new SelectElement(allDate[2]);
+                selectElement2.SelectByIndex(RandNumber(22, 52));
+                Thread.Sleep(1212);
+
+                var button2 = driver.FindElement(By.ClassName("lC6p0"));
+                button2.Click();
+                Thread.Sleep(256);
+
+
+                //Login Yandex
+                //IWebDriver yandexDriver = new ChromeDriver(_chromeDriverService, _chromeOptions);
+                label1.Text = "Yandexe giriş yapılıyor...";
+
+                yandexDriver.Navigate().GoToUrl("https://passport.yandex.com.tr/auth?from=mail&origin=hostroot_homer_auth_tr&retpath=https%3A%2F%2Fmail.yandex.com.tr%2F&backpath=https%3A%2F%2Fmail.yandex.com.tr%3Fnoretpath%3D1");
+                Thread.Sleep(15256);
+                try
+                {
+                    var yandexMailLogin = yandexDriver.FindElement(By.Id("passp-field-login"));
+                    yandexMailLogin.SendKeys(yandexMail);
+                    Thread.Sleep(1231);
+                }
+                catch
+                {
+                    yandexDriver.Navigate().Refresh();
+                    Thread.Sleep(15256);
+                    var yandexMailLogin = yandexDriver.FindElement(By.Id("passp-field-login"));
+                    yandexMailLogin.SendKeys(yandexMail);
+                }
+
+                label1.Text = "Yandex mail girildi...";
+
+                var buttonLoginYandex = yandexDriver.FindElement(By.Id("passp:sign-in"));
+                buttonLoginYandex.Click();
+                Thread.Sleep(3531);
+
+                var yandexMailPass = yandexDriver.FindElement(By.Id("passp-field-passwd"));
+                yandexMailPass.SendKeys(yandexPass);
+                Thread.Sleep(1538);
+                label1.Text = "Yandex şifre girildi...";
+
+                var buttonLoginYandexNext = yandexDriver.FindElement(By.Id("passp:sign-in"));
+                buttonLoginYandexNext.Click();
+
+                label1.Text = "Yandex mail'e gidiliyor...";
+                Thread.Sleep(8000);
+
+                //navigate social page 
+                yandexDriver.Navigate().GoToUrl("https://mail.yandex.com.tr/#tabs/social");
+                Thread.Sleep(11538);
+
+
+                try
+                {
+                    var popupClose = yandexDriver.FindElement(By.CssSelector("div.b-popup__close.ns-action"));
+                    popupClose.Click();
+                }
+                catch (Exception e)
+                {
+                    Thread.Sleep(1538);
+                }
+                label1.Text = "Yandexten onay kodu alınıyor...";
+
+                var buttonSelectHrefCode = yandexDriver.FindElements(By.ClassName("mail-MessageSnippet-Wrapper"));
+                buttonSelectHrefCode[0].Click();
+                Thread.Sleep(4523);
+                label1.Text = "Onay kodu alındı...";
+
+                var yandexCodeGet = yandexDriver.FindElements(By.TagName("td"));
+                //yandexCode[8]; -->> all text
+                //yandexCode[9]; -->> all text
+                //yandexCode[16]; -->> İnstagram confirmation code
+                String yandexCode = yandexCodeGet[16].Text;
+                Thread.Sleep(1000);
+                //yandex done
+
+                label1.Text = "Onay kodu giriliyor...";
+
+                //send keys for instagram register
+                WebElement emailConfirmationCode = (WebElement)driver.FindElement(By.Name("email_confirmation_code"));
+                emailConfirmationCode.SendKeys(yandexCode);
+
+                Thread.Sleep(1000);
+
+                var registerButton = driver.FindElement(By.ClassName("L3NKy"));
+                registerButton.Click();
+
+
+
+                if(yandexCode.Length>4)
+                {
+                    label1.Text = "Sonuç başarılı..";
+
+                    //add user to txt
+                    WriteInstaAccount(instaUsername, instaPassword, yandexMail, instaFullName);
+                    //set success text
+                    String up = label4.Text;
+                    int sayi = int.Parse(up) + 1;
+                    label4.Text = sayi.ToString();
+
+                    //add successMail.txt
+                    WriteFileSuccess(yandexMail, yandexPass);
+                }
+                else
+                {
+                    label1.Text = "Sonuç başarısız..";
+                    // set failed text
+                    String up = label5.Text;
+                    int sayi = int.Parse(up) + 1;
+                    label5.Text = sayi.ToString();
+                    //add failMail.txt
+                    WriteFileFail(yandexMail, yandexPass);
+                }
+
+                Thread.Sleep(30000);
+
+
+            }
+            catch (Exception e)
+            {
+                label1.Text = "İnternet bağlantısı, proxy veya mailleri kontrol edin...";
+                // set failed text
+                String up = label5.Text;
+                int sayi = int.Parse(up) + 1;
+                label5.Text = sayi.ToString();
+                //add failMail.txt
+                WriteFileFail(yandexMail, yandexPass);
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                driver.Quit();
+                yandexDriver.Quit();
+
+            }
+        }
+        public async void Start(String yandexMail,String yandexPass)
+        {
+            String instaFullName="";
+            try
+            {
+                string[] lines = File.ReadAllLines("fullname.txt");
+                instaFullName = lines[RandNumber(0,490)].ToLower();
+
+            }
+            catch (IOException e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            String instaUsername = CreateUsername(yandexMail);
+            String instaPassword = yandexPass;
+            label1.Text = "Rastgele kullanıcı adı oluşturuldu...";
+
+            Deneme(yandexMail, yandexPass, instaFullName, instaPassword,instaUsername);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            try
+            {
+                label1.Text = "Bot durumu: Aktif..";
+                string[] allMail = File.ReadAllLines("mail.txt");
+                foreach(String mail in allMail)
+                {
+                    String[] splited = mail.Split(' ');
+                    /* 
+                     * Mail Adresi: seym4ali@yandex.com Şifre: ruvali8226 Güvenlik Sorusu Cevabı: gazapizm
+                     */
+                    //splited[0]= Mail 
+                    //splited[1] = Adresi:  
+                    //splited[2] = mail@yandex.com  
+                    //splited[3] = Şifre:  
+                    //splited[4] = password  
+                    label1.Text = "Yandex mail alınıyor...";
+
+                    String yandexMail = splited[2].Trim();
+                    String yandexPass = splited[4].Trim();
+
+                    //start create mail
+                    Start(yandexMail, yandexPass);
+                }
+                MessageBox.Show("Tüm yandex hesaplar kullanıldı");
+            }
+            catch (Exception a)
+            {
+            }
+        }
+    }
+}
